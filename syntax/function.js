531f4a519282c875bb73b56cd58d8d9a28cde05a
hwk0911@gmail.com <hwk0911@gmail.com>
2020-04-29 17:16:09 +0900

    함수 생성 및 사용

---
+++ syntax/function.js
@@ -0,0 +1,22 @@
+console.log('A');
+console.log('A');
+console.log('A');
+console.log('A');
+test();
+console.log('A');
+console.log('A');
+console.log('A');
+console.log('A');
+test();
+console.log('A');
+console.log('A');
+console.log('A');
+console.log('A');
+test();
+
+
+function test () {
+    console.log(3);
+    console.log(4);
+    console.log(5);    
+}
