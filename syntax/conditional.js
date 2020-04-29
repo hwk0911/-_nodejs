e91f9cfadeb375be5213765a8b793da94b351252
hwk0911@gmail.com <hwk0911@gmail.com>
2020-04-29 15:57:43 +0900

    조건문 숙달, 파라미터 전달방식 확인

---
+++ syntax/conditional.js
@@ -0,0 +1,15 @@
+var args = process.argv;
+
+var a = args[2];
+
+
+console.log('A');
+console.log('B');
+
+if (a == "true") {
+    console.log('C1');
+} else {
+    console.log('C2');
+    
+}
+console.log('D');
