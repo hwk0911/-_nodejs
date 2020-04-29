f37449a8083eed2dc34a8c1a0caa88d0cd539bcb
hwk0911@gmail.com <hwk0911@gmail.com>
2020-04-29 17:20:01 +0900

    함수 활용

---
+++ syntax/function2.js
@@ -0,0 +1,16 @@
+console.log(Math.round(1.5));
+console.log(Math.round(1.4));
+console.log(Math.round(1.2));
+console.log(Math.round(1.6));
+
+console.log(sum(2, 4));
+
+function sum (a, b) {
+    return a + b;
+}
+
+/*
+sum (2, 4) = arguments
+function sum (a, b) = parameter
+
+*/
