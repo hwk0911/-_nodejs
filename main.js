23183fb2537080263e8125c021ad43426fbcccfa
hwk0911 <hwk0911@gmail.com>
2020-05-01 17:55:57 +0900

    SSD 고장으로 인한 복구 및 수정 적용

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ var app = http.createServer(function (request, response) {
         var description = 'Hello, Node.js';
         var list = '<ul>';
         var i = 0;
-        
+
         while (i < filelist.length) {
           list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
           i = i + 1;
         }
-        
+
         list = list + '</ul>';
-        
+
         var template = `
           <!doctype html>
           <html>
