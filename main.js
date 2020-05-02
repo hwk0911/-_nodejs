edc35686bb5367078c800713d967c82ea5e37708
hwk0911 <hwk0911@gmail.com>
2020-05-02 21:28:50 +0900

    ADD : 입력받은 데이터파일 생성
    입력받은 post 데이터를 기준으로 새로운 파일을 생성한다.

--- main.js
+++ main.js
@@ -88,18 +88,19 @@ var app = http.createServer(function (request, response) {
   }
   else if (pathname === '/create_process') {
     var body = '';
-    request.on('data', function(data) {
+    request.on('data', function (data) {
       body += data;
     });
-    request.on('end', function() {
+    request.on('end', function () {
       var post = qs.parse(body);
       var title = post.title;
       var description = post.description;
-      console.log(post);
+      fs.writeFile(`data/${title}`, description, 'utf8',
+        function (err) {
+          response.writeHead(302, {Location : `/?id=${title}`});
+          response.end('sucess');          
+        })
     });
-
-    response.writeHead(200);
-    response.end('sucess');
   }
   else {
     response.writeHead(404);
