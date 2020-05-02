0a77a9550e7702b02580adcc41c73d8b8baa842e
hwk0911 <hwk0911@gmail.com>
2020-05-02 21:23:13 +0900

    send the post data

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
   return `
@@ -68,7 +69,7 @@ var app = http.createServer(function (request, response) {
       var description = 'Hello, Node.js';
       var list = templateList(filelist);
       var template = templateHTML(title, list, `
-      <form action="http://localhost:3000/process_create" method = "post">
+      <form action="http://localhost:3000/create_process" method = "post">
     <p>
         <input type="text" name = "title" placeholder = "title">
     </p>
@@ -85,7 +86,21 @@ var app = http.createServer(function (request, response) {
       response.end(template);
     })
   }
+  else if (pathname === '/create_process') {
+    var body = '';
+    request.on('data', function(data) {
+      body += data;
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      console.log(post);
+    });
 
+    response.writeHead(200);
+    response.end('sucess');
+  }
   else {
     response.writeHead(404);
     response.end('Not found');
